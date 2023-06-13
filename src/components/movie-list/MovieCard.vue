047995e76d6ca7f6bfd0659215919a57a47329c4
L1ka <l.gvaramashvili1@gmail.com>
2023-06-13 16:19:54 +0400

    chore: movies list page marckup

---
+++ src/components/movie-list/MovieCard.vue
@@ -0,0 +1,19 @@
+<script setup>
+import IconComment from '@/components/icons/IconComment.vue'
+</script>
+
+<template>
+  <div class="text-white">
+    <div
+      class="w-full h-[250px] md:h-[350px] lg:h-[400px] xl:h-[450px] bg-cover bg-no-repeat bg-center rounded-md mb-4"
+      style="background-image: url('src/assets/images/eliotmovie.png')"
+    ></div>
+
+    <p class="mb-4 text-sm-bold">Loki Mobius (2021)</p>
+
+    <div class="flex items-center">
+      <p class="mr-2">10</p>
+      <icon-comment></icon-comment>
+    </div>
+  </div>
+</template>
---
+++ src/views/MovieListPage.vue
@@ -0,0 +1,28 @@
+<script setup>
+import MovieCard from '@/components/movie-list/MovieCard.vue'
+import MovieListHeader from '@/components/movie-list/MovieListHeader.vue'
+import NavigationBar from '@/components/news-feed/NavigationBar.vue'
+import SideBar from '@/components/news-feed/SideBar.vue'
+</script>
+
+<template>
+  <div class="flex flex-col items-center lg:items-end min-h-screen w-screen">
+    <navigation-bar></navigation-bar>
+
+    <side-bar class="hidden lg:block"></side-bar>
+    <movie-list-header></movie-list-header>
+    <div
+      class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10 w-[100%] lg:w-[75%] xl:w-[80%] lg:my-28 my-10 px-8 md:px-12"
+    >
+      <div><movie-card></movie-card></div>
+      <div><movie-card></movie-card></div>
+      <div><movie-card></movie-card></div>
+      <div><movie-card></movie-card></div>
+      <div><movie-card></movie-card></div>
+      <div><movie-card></movie-card></div>
+      <div><movie-card></movie-card></div>
+      <div><movie-card></movie-card></div>
+      <div><movie-card></movie-card></div>
+    </div>
+  </div>
+</template>
