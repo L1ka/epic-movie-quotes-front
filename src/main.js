c6768be36951614cb8d7c15b18b1d5e58d27e7c8
L1ka <l.gvaramashvili1@gmail.com>
2023-06-15 15:41:21 +0400

    chore: config files

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import '@/assets/main.css'
 import i18n from '@/plugins/index.js'
 import '@/config/vee-validate/rules.js'
 import '@/config/vee-validate/messages.js'
+import axiosInstance from '@/config/axios/index.js'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -14,5 +15,6 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(i18n)
+app.config.globalProperties.$axios = axiosInstance
 
 app.mount('#app')
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
@@ -12,10 +9,27 @@ module.exports = {
         'dark-gray': '#6C757D',
         'base-red': '#E31221',
         blue: '#0D6EFD',
-        'light-gray': '#222030'
+        'light-gray': '#222030',
+        'light-yallow': '#DDCCAA',
+        green: '#198754',
+        border: '#efefef4d',
+        'light-black': '#24222F',
+        'modal-black': '#11101A',
+        'light-green': '#198754',
+        purple: '9747FF'
       },
-    },
+      fontSize: {
+        sm: ['16px', { lineHeight: '24px', fontWeight: '400' }],
+        'sm-light': ['20px', { lineHeight: '30px', fontWeight: '400' }],
+        'sm-dark': ['16px', { lineHeight: '24px', fontWeight: '700' }],
+        'sm-bold': ['24px', { lineHeight: '29px', fontWeight: '500' }],
+        'sm-m': ['20px', { lineHeight: '30px', fontWeight: '700' }],
+        m: ['32px', { lineHeight: '38px', fontWeight: '500' }],
+        'm-bold': ['30px', { lineHeight: '45px', fontWeight: '700' }],
+        lg: ['50px', { lineHeight: '75px', fontWeight: '700' }],
+        '2lg': ['60px', { lineHeight: '90px', fontWeight: '700' }]
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
