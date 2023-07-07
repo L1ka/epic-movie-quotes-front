90a56bdec130e3c4ff718a2843f65628abd97587
L1ka <l.gvaramashvili1@gmail.com>
2023-07-07 15:51:57 +0400

    chore: user card and search components

---
+++ src/components/news-feed/UserCard.vue
@@ -0,0 +1,34 @@
+<script setup>
+import IconFilledHeart from '@/components/icons/IconFilledHeart.vue'
+import IconComment from '@/components/icons/IconComment.vue'
+
+const backUrl = import.meta.env.VITE_API_BASE_URL
+const props = defineProps({
+  user: { type: Object, required: true },
+  like: { type: Boolean, required: false },
+  comment: { type: Boolean, required: false },
+  seen: { type: [Boolean, Number], required: false }
+})
+</script>
+
+<template>
+  <div class="flex items-center mb-4 mt-8" v-if="user">
+    <div
+      :style="{ 'background-image': 'url(' + backUrl + user.image + ')' }"
+      class="w-14 h-14 mr-4 bg-cover bg-no-repeat bg-center rounded-full mb-2"
+      :class="seen == 0 && (like || comment) ? 'border-2 border-green' : ''"
+    ></div>
+    <div>
+      <p class="text-white text-sm lg:text-sm-bold capitalize mb-3">{{ user.first_name }}</p>
+      <div class="flex mb-3" v-if="like">
+        <icon-filled-heart class="mr-3"></icon-filled-heart>
+        <p>Reacted to your quote</p>
+      </div>
+
+      <div class="flex" v-if="comment">
+        <icon-comment class="mr-3"></icon-comment>
+        <p>Commented to your quote..</p>
+      </div>
+    </div>
+  </div>
+</template>
