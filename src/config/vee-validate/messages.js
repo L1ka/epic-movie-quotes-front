62dfd1bfeb184e905ff93870a27caf1a935ba0f3
L1ka <l.gvaramashvili1@gmail.com>
2023-07-13 17:41:14 +0400

    chore: custom rule for validation

--- src/config/vee-validate/messages.js
+++ src/config/vee-validate/messages.js
@@ -10,10 +10,13 @@ configure({
       messages: {
         required: 'This field is required',
         min: '{field} should contain at least 0:{min} character',
-        // lower_case: 'use lower case characters',
         alpha_num_lower: 'use lower case alphanumerical Latin characters',
+        geo_num: 'use georgian letters',
         max: '{field} can contain max 0:{max} character',
-        confirmed: 'confirm password does not match password'
+        confirmed: 'confirm password does not match password',
+        alpha: 'use Latin characters',
+        alpha_latin: 'use Latin characters',
+        numeric: 'should contain 4 digit number'
       },
       names: {
         first_name: 'name',
@@ -27,9 +30,13 @@ configure({
         min: '{field}ს ველი უნდა შედგებოდეს მინიმუმ 0:{min} სიმბოლოსგან',
         max: '{field}ს ველი უნდა შედგებოდეს მინიმუმ 0:{max} სიმბოლოსგან',
         email: 'თქვენ მიერ შეყვანილი მეილი არასწორია',
-        // lower_case: 'გამოიყენეთ დაბალი რეგისტრის სიმბოლოები',
         alpha_num_lower: 'გამოიყენეთ დაბალი რეგისტრის ლათინური სიმბოლოები და რიცხვები',
-        confirmed: 'განმეორებითი პაროლი არ ემთხვევა პაროლს'
+        geo_num: 'გამოიყენეთ ქართული სიმბოლოები',
+        confirmed: 'განმეორებითი პაროლი არ ემთხვევა პაროლს',
+        alpha: 'გამოიყენეთ ქართული სიმბოლოები',
+        numeric: 'აღნიშნული ველი უნდა შედგებოდეს 4 ციფრისგან',
+        image: 'ფოტოს ფორმატი არასწორია',
+        alpha_geo: 'გამოიყენეთ ლათინური სიმბოლოები'
       },
       names: {
         email: 'ელ.ფოსტი',
--- src/config/vee-validate/rules.js
+++ src/config/vee-validate/rules.js
@@ -1,5 +1,16 @@
 import { defineRule } from 'vee-validate'
-import { required, min, email, max, alpha_num, confirmed } from '@vee-validate/rules'
+import {
+  required,
+  min,
+  email,
+  max,
+  alpha_num,
+  confirmed,
+  alpha,
+  numeric,
+  image,
+  one_of
+} from '@vee-validate/rules'
 
 defineRule('required', required)
 defineRule('min', min)
@@ -7,9 +18,32 @@ defineRule('max', max)
 defineRule('email', email)
 defineRule('alpha_num', alpha_num)
 defineRule('confirmed', confirmed)
+defineRule('alpha', alpha)
+defineRule('numeric', numeric)
+defineRule('image', image)
+defineRule('one_of', one_of)
 
 defineRule('alpha_num_lower', (value) => {
+  if (!value) {
+    return true
+  }
   if (!/^[a-z0-9]+$/.test(value)) {
+    return false
+  }
+
+  return true
+})
+
+defineRule('alpha_geo', (value) => {
+  if (!/^[ა-ჰ\s?,.'":;]+$/.test(value)) {
+    return false
+  }
+
+  return true
+})
+
+defineRule('alpha_latin', (value) => {
+  if (!/^[a-zA-Z\s?,.'":;]+$/.test(value)) {
     return false
   }
 
