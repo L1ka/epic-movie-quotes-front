d4c248dce683cb645fc489625bbf1cbef6683208
L1ka <l.gvaramashvili1@gmail.com>
2023-07-15 02:08:45 +0400

    chore: handle errors

--- src/config/axios/index.js
+++ src/config/axios/index.js
@@ -13,4 +13,16 @@ const axiosInstance = axios.create({
   }
 })
 
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === (500 || 419 || 401)) {
+      console.log('500 erroooor')
+      const frontUrl = import.meta.env.VITE_FRONT_BASE_URL
+      window.location.replace(`${frontUrl}/403`)
+    }
+    return Promise.reject(error)
+  }
+)
+
 export default axiosInstance
